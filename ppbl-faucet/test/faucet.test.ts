2fe61b4160e0a8acb4e7ae463060f1075737c2f8
Lawrence <lawrence.ley@contextsolutions.ca>
2025-01-29 12:03:40 -0500

    - added one-shot.ak minting policy to create dummy access and faucet tokens - added mint, lock, withdraw and check balance test cases - removed extra console log outputs

--- ppbl-faucet/test/faucet.test.ts
+++ ppbl-faucet/test/faucet.test.ts
@@ -1,9 +1,8 @@
 import { describe, expect, it } from '@jest/globals';
 import { 
-    //MeshTx,
+    MeshTx,
     newWallet,
-    provider,
-    sleep
+    provider
 } from "./offchain";
 
 /*
@@ -26,6 +25,12 @@ const networkId = 0; // set to 0 for testnet, 1 for mainnet
 
 describe('E2E Faucet Test', () => {
     console.log("Starting E2E Faucet Test");
+    let accessTokenHex: string;
+    let accessTokenPolicy: string;
+    let faucetTokenHex: string;
+    let faucetTokenPolicy: string;
+    const withdrawalAmount = 100n;
+    const faucetLockedAmount = 1000000n;
 
     beforeAll(async () => {
 const address = wallet.getChangeAddress();
@@ -35,19 +40,68 @@ const address = wallet.getChangeAddress();
     });
 
     afterAll(async () => {
-        //await teardown();
         console.log("Counter Test Finished");
     }); 
 
-
-    it('mint access token', async() => {
-        console.log('\n--- Before Mint Tx ---');
-        //const tx = new MeshTx(wallet, provider, networkId);
-        //const txHash = await tx.mint(tokenName, quantity);
-        console.log('\n--- After Mint Tx ---');
-        //console.log({ txHash });
-        //await sleep(2);
+    it('Mint access token', async() => {
+        console.log('\n--- Before Mint Access Token Tx ---');
+        const tx = new MeshTx(wallet, provider, networkId);
+        const result = await tx.mint("access-token", 1n);
+        console.log('\n--- After Mint Access Token Tx ---');
+        console.log({ result });
+        accessTokenHex = result.tokenNameHex;
+        accessTokenPolicy = result.mintTokenPolicy;
+        await sleep(2);
     }, 100000);
 
+    it('Mint faucet tokens', async() => {
+        console.log('\n--- Before Mint Faucet Token Tx ---');
+        const tx = new MeshTx(wallet, provider, networkId);
+        const result = await tx.mint("faucet-token", 1000000n);
+        console.log('\n--- After Mint Faucet Token Tx ---');
+        console.log({ result });
+        faucetTokenHex = result.tokenNameHex;
+        faucetTokenPolicy = result.mintTokenPolicy;
+        await sleep(2);
+    }, 100000);
+
+    it('Lock faucet tokens', async() => {
+        console.log('\n--- Before Lock Faucet Token Tx ---');
+        const tx = new MeshTx(wallet, provider, networkId);
+        const result = await tx.lock(withdrawalAmount, faucetLockedAmount, faucetTokenHex, faucetTokenPolicy, accessTokenPolicy);
+        console.log('\n--- After Lock Faucet Token Tx ---');
+        console.log({ result });
+        await sleep(2);
+    }, 100000);
+
+    it('Withdrawal Faucet Token', async() => {
+        console.log('\n--- Before Withdrawal Faucet Token Tx ---');
+        const tx = new MeshTx(wallet, provider, networkId);
+        const result = await tx.withdrawal(withdrawalAmount, faucetTokenHex, faucetTokenPolicy, accessTokenHex, accessTokenPolicy);
+        console.log('\n--- After Withdrawal Faucet Token Tx ---');
+        console.log({ result });
+        await sleep(2);
+    }, 100000);
+
+    it('Withdrawal Faucet Token', async() => {
+        console.log('\n--- Before Withdrawal Faucet Token Tx ---');
+        const tx = new MeshTx(wallet, provider, networkId);
+        const result = await tx.withdrawal(withdrawalAmount, faucetTokenHex, faucetTokenPolicy, accessTokenHex, accessTokenPolicy);
+        console.log('\n--- After Withdrawal Faucet Token Tx ---');
+        console.log({ result });
+        await sleep(2);
+    }, 100000);
+
+    it('Check Wallet Balance', async() => {
+        const balance = await wallet.getBalance();
+        console.log({ balance });
+        expect(balance.find(b => b.unit === faucetTokenPolicy + faucetTokenHex)?.quantity).toBe('200');
+        expect(balance.find(b => b.unit === accessTokenPolicy + accessTokenHex)?.quantity).toBe('1');
+    });
+
 }); 
+
+const sleep = (second: number) =>
+    new Promise((resolve) => setTimeout(resolve, second * 1000));
+
 
--- ppbl-faucet/test/offchain.ts
+++ ppbl-faucet/test/offchain.ts
@@ -1,7 +1,6 @@
 import {
     applyCborEncoding,
     applyParamsToScript,
-    fromLovelace,
     parseDatumCbor
 } from "@meshsdk/core-csl";
 import {
@@ -18,7 +17,6 @@ import {
     stringToHex,
     outputReference,
     resolveScriptHash,
-    PubKeyHash,
     Integer,
     ConStr0,
 } from "@meshsdk/core";
@@ -58,6 +56,270 @@ export const newWallet = (providedMnemonic?: string[]) => {
   return wallet;
 };
 
-export const sleep = (seconds: number) => {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-}; 
+const mintContractCbor = (
+    tokenNameHex: string,
+    utxoTxHash: string,
+    utxoTxId: number,
+  ) => {
+    let scriptCbor = blueprint.validators[1]!.compiledCode;
+    let utxo = outputReference(utxoTxHash, utxoTxId);
+
+    return applyParamsToScript(
+      scriptCbor,
+      [builtinByteString(tokenNameHex), utxo],
+      "JSON",
+    );
+  };
+
+const faucetContractCbor = (tokenNameHex: string, policyId: string) => {
+    let scriptCbor = blueprint.validators[2]!.compiledCode;
+    return applyParamsToScript(scriptCbor, [tokenNameHex, policyId]);
+};
+
+export class MeshTx {
+    constructor(
+        public wallet: MeshWallet, 
+        //public provider: BlockfrostProvider,
+        public provider: YaciProvider,
+        public networkId: number,
+        public stakeCredential?: string | null,
+        
+    ) {
+        this.wallet = wallet;
+        this.provider = provider;
+        this.networkId = networkId;
+        this.stakeCredential = stakeCredential ?? null;
+    }
+  
+    newTx = async () => {
+      const address = this.wallet.getChangeAddress();
+  
+      const txBuilder = new MeshTxBuilder({
+        fetcher: this.provider,
+        evaluator: this.provider,
+      });
+      const utxos = await this.wallet.getUtxos();
+      txBuilder.changeAddress(address).selectUtxosFrom(utxos);
+      return txBuilder;
+    };
+  
+    newValidationTx = async () => {
+      const txBuilder = await this.newTx();
+      const collateral = (await this.wallet.getCollateral())[0];
+      if (collateral) {
+      txBuilder.txInCollateral(
+        collateral.input.txHash,
+        collateral.input.outputIndex,
+        collateral.output.amount,
+        collateral.output.address
+        );
+        return txBuilder;
+      } else {
+        throw new Error("No collateral available");
+      }
+    };
+
+    getScriptAddress = (scriptCbor: string) => {
+        const { address } = serializePlutusScript(
+          { code: scriptCbor, version: languageVersion },
+          this.stakeCredential ?? undefined,
+          this.networkId,
+        );
+        return address;
+      };
+
+    mint = async (
+        tokenName: string,
+        quantity: bigint,
+    ) => {
+        try {
+            const walletAddress = this.wallet.getChangeAddress();
+            console.log("WalletAddress:", walletAddress);
+            const tokenNameHex = stringToHex(tokenName);
+            const utxos = await this.provider.fetchAddressUTxOs(walletAddress);
+            if (utxos.length < 1) throw new Error("No UTXOs available");
+            const firstUtxo = utxos[0];
+
+            const mintTokenScript = mintContractCbor(
+                tokenNameHex,
+                firstUtxo?.input.txHash ?? '',
+                firstUtxo?.input.outputIndex ?? 0,
+              );
+            
+            const mintTokenPolicy = resolveScriptHash(
+                mintTokenScript,
+                languageVersion,
+              );
+
+            // Log for debugging
+            console.log("Building mint transaction...");
+            console.log("MintTokenPolicy:", mintTokenPolicy);
+            console.log("TokenNameHex:", tokenNameHex);
+            console.log("Quantity:", quantity);
+            const txBuilder = await this.newValidationTx();
+            const txHex = await txBuilder
+                .txIn(
+                    firstUtxo?.input.txHash ?? '',
+                    firstUtxo?.input.outputIndex ?? 0,
+                    firstUtxo?.output.amount ?? [],
+                    firstUtxo?.output.address ?? '',
+                )
+                .mintPlutusScript(languageVersion)
+                .mint(quantity.toString(), mintTokenPolicy, tokenNameHex)
+                .mintingScript(mintTokenScript)
+                .mintRedeemerValue(mConStr0([]))
+                .txOut(walletAddress, [
+                    { unit: "lovelace", quantity: "2000000" },
+                    { unit: mintTokenPolicy + tokenNameHex, quantity: quantity.toString() },
+                  ])
+                .txOut(walletAddress, [
+                    { unit: "lovelace", quantity: "5000000" },
+                  ]) /// leave 5 ada for collateral
+                .complete();
+
+            console.log("Transaction built successfully");
+            const singedTx = await this.wallet.signTx(txHex);
+            const txHash = await this.wallet.submitTx(singedTx);
+            return {
+                txHash,
+                tokenNameHex,
+                mintTokenPolicy,
+            };
+        } catch (error) {
+            console.error("Error in mint transaction:", error);
+            throw error;
+        }
+    };
+
+    lock = async (
+        withdrawalAmount: bigint,
+        faucetLockedAmount: bigint,
+        faucetTokenNameHex: string,
+        faucetTokenPolicy: string,
+        accessTokenPolicy: string,
+    ) => {
+        try {
+            const walletAddress = this.wallet.getChangeAddress();
+            const faucetContractScript: PlutusScript = {
+                code: faucetContractCbor(accessTokenPolicy, faucetTokenPolicy),
+                version: languageVersion,
+              };
+            const faucetScriptCbor = applyCborEncoding(faucetContractScript.code);
+            const faucetScriptAddress = this.getScriptAddress(faucetScriptCbor);
+
+            // Log for debugging
+            console.log("Building lock transaction with datum...");
+            console.log("ValidatorAddress:", faucetScriptAddress);
+            
+            const txBuilder = await this.newTx();
+            const txHex = await txBuilder
+                .txOut(
+                    faucetScriptAddress, [
+                        { unit: "lovelace", quantity: "2000000" },
+                        { unit: faucetTokenPolicy + faucetTokenNameHex, quantity: faucetLockedAmount.toString() },
+                    ]
+                )
+                .txOutInlineDatumValue(mConStr0([withdrawalAmount, faucetTokenNameHex]))
+                .txOut(walletAddress, [
+                    { unit: "lovelace", quantity: "5000000" },
+                  ]) /// leave 5 ada for collateral
+                .complete();
+
+            console.log("Transaction built successfully");
+            const singedTx = await this.wallet.signTx(txHex);
+            const txHash = await this.wallet.submitTx(singedTx);
+            return {
+                txHash
+            }
+        } catch (error) {
+            console.error("Error in lock transaction:", error);
+            throw error;
+        }
+    };
+  
+    withdrawal = async (
+        withdrawalAmount: bigint,
+        faucetTokenNameHex: string,
+        faucetTokenPolicy: string,
+        accessTokenNameHex: string,
+        accessTokenPolicy: string,
+
+    ) => {
+        try {
+            let datum: ConStr0<[Integer, string]> | null = null;
+            let faucetAmount: string | undefined;
+            const walletAddress = this.wallet.getChangeAddress();
+            const ownPubKey = deserializeAddress(walletAddress).pubKeyHash;
+            const faucetContractScript: PlutusScript = {
+                code: faucetContractCbor(accessTokenPolicy, faucetTokenPolicy),
+                version: languageVersion,
+              };
+            const faucetScriptCbor = applyCborEncoding(faucetContractScript.code);
+            const faucetScriptAddress = this.getScriptAddress(faucetScriptCbor);
+            console.log("FaucetScriptAddress:", faucetScriptAddress);
+
+            const scriptInput = (
+                await this.provider.fetchAddressUTxOs(faucetScriptAddress)
+              ).find((input) => {
+                if (input.output.plutusData) {
+                  datum = parseDatumCbor(input.output.plutusData);
+                  faucetAmount = input.output.amount.find((amount) => amount.unit === faucetTokenPolicy + faucetTokenNameHex)?.quantity;
+                  return input;
+                }
+                return null;
+              });
+
+            if (!datum) throw new Error("No datum found");
+            console.log("Current FaucetAmount:", faucetAmount);
+            const faucetAmountInt = BigInt(faucetAmount ?? 0);
+            const remainingFaucetAmount = faucetAmountInt - withdrawalAmount;
+            const txBuilder = await this.newValidationTx();
+
+            // Log for debugging
+            console.log("Building transaction to increment datum...");
+            console.log("PubKeyHash:", ownPubKey);
+            console.log("Datum:", datum);
+
+            const txHex = await txBuilder
+                .spendingPlutusScriptV3()
+                .txIn(
+                    scriptInput!.input.txHash,
+                    scriptInput!.input.outputIndex,
+                    scriptInput!.output.amount,
+                    scriptInput!.output.address
+                )
+                .txInInlineDatumPresent()
+                .txInScript(faucetScriptCbor)
+                .txInRedeemerValue(mConStr0([ownPubKey, accessTokenNameHex]))
+                .txOut(
+                    faucetScriptAddress, [
+                        { unit: "lovelace", quantity: "2000000" },
+                        { unit: faucetTokenPolicy + faucetTokenNameHex, quantity: remainingFaucetAmount.toString() },
+                    ]
+                )
+                .txOutInlineDatumValue(mConStr0([withdrawalAmount, faucetTokenNameHex]))
+                .txOut(
+                    walletAddress, [
+                        { unit: "lovelace", quantity: "2000000" },
+                        { unit: faucetTokenPolicy + faucetTokenNameHex, quantity: withdrawalAmount.toString() },
+                        { unit: accessTokenPolicy + accessTokenNameHex, quantity: "1" }
+                    ]
+                )
+                .txOut(walletAddress, [
+                    { unit: "lovelace", quantity: "5000000" },
+                  ]) /// leave 5 ada for collateral
+                .requiredSignerHash(ownPubKey)
+                .complete();
+
+            console.log("Transaction built successfully");
+            const singedTx = await this.wallet.signTx(txHex);
+            const txHash = await this.wallet.submitTx(singedTx);
+            return {
+                txHash
+            }
+        } catch (error) {
+            console.error("Error in withdrawal transaction:", error);
+            throw error;
+        }
+    };
+  }
